21b8d1e11430583414b6eb403e7e87c91c03ba1d
Dippimunch <dippimunch@aim.com>
2020-12-06 04:39:48 -0500

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,7 +1,20 @@
-file = open(r"day1_data.txt", "r")
-
+file = open(r"day1_data2.txt", "r")
+data = file.read()
+words = data.split()
+#print(words)
 def report(data):
-    for i in range(len(data)):
-        sum1 = i
-        for j in range(len(data)):
-            sum2 = j
+    for i in range(len(words)):
+        sum1 = int(words[i])
+        for j in range(len(words)):
+            sum2 = int(words[j])
+            value = sum1 + sum2
+            #print(value)
+            #print(sum1)
+            #print(int(sum1) * int(sum2))
+            if value == 2020:
+                solution = sum1 * sum2
+                print("%i + %i" % (sum1, sum2))
+                #print(solution)
+
+print('Number of words in text file :', len(words))
+report(file)
