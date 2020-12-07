27e4f84ade37e066b901b6d9f900cad6197fed45
Dippimunch <dippimunch@aim.com>
2020-12-06 23:59:37 -0500

    Add files via upload

--- day2.py
+++ day2.py
@@ -4,6 +4,14 @@ import re
 file = open(r'day2_data2.txt', 'r')
 #data = file.readlines()
 passwords = re.findall(r'(\d+)-(\d+)\s(\w)', file.read())
+valid = 0
+
+for tuple in passwords:
+    print(tuple[0], tuple[1], tuple[2])
+
+    #if passwords[tuple]
+    
+    
 
 #print(file.read())
-print(passwords)
+#print(passwords)
