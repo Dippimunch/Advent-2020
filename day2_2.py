0b55d8cac1e710c5b2a2d5919ae73fa8e5a8c41b
Dippimunch <dippimunch@aim.com>
2020-12-07 03:30:03 -0500

    Add files via upload
    gotemmm

--- day2_2.py
+++ day2_2.py
@@ -1,23 +1,12 @@
 import re
 
 
-file = open(r'day2_data2.txt', 'r')
+file = open(r'day2_data.txt', 'r')
 passwords = re.findall(r'(\d+)-(\d+)\s(\w):\s(\w+)', file.read())
 valid = 0
 
 for i in range(len(passwords)):
-    #valid_check = passwords[i][3].count(passwords[i][2])
-
-    #if passwords[i][3][int(passwords[i][0])] == passwords[i][2] ^ passwords[i][3][int(passwords[i][1])-1] == passwords[i][2]:
-    #print(passwords
-    print(passwords[i][3][int(passwords[i][0])-1] == passwords[i][2])
-    print(passwords[i][3][int(passwords[i][1])-1] == passwords[i][2])
-
-    print(passwords[i][3][int(passwords[i][0])-1] == passwords[i][2] ^ passwords[i][3][int(passwords[i][1])-1] == passwords[i][2])
-
-    #print(passwords[i][3][int(passwords[i][0])], passwords[i][3][int(passwords[i][1])-1])
-
-    """if valid_check >= int(passwords[i][0]) and valid_check <= int(passwords[i][1]):
-        valid += 1"""
+    if bool(passwords[i][3][int(passwords[i][0])-1] == passwords[i][2]) ^ bool(passwords[i][3][int(passwords[i][1])-1] == passwords[i][2]):
+        valid += 1
 
 print('# of valid passwords: ', valid)
