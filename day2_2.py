9c90243814e9864b545aed10685e36231e1e8a7f
Dippimunch <dippimunch@aim.com>
2020-12-07 00:50:56 -0500

    Add files via upload

---
+++ day2_2.py
@@ -0,0 +1,15 @@
+import re
+
+
+file = open(r'day2_data2.txt', 'r')
+passwords = re.findall(r'(\d+)-(\d+)\s(\w):\s(\w+)', file.read())
+valid = 0
+
+for i in range(len(passwords)):
+    #valid_check = passwords[i][3].count(passwords[i][2])
+    print(passwords[i][3][int(passwords[i][0])])
+
+    """if valid_check >= int(passwords[i][0]) and valid_check <= int(passwords[i][1]):
+        valid += 1"""
+
+print('# of valid passwords: ', valid)
