88715737e8942b6ce0251182e84b8a16e4751dc3
Dippimunch <dippimunch@aim.com>
2020-12-07 00:43:11 -0500

    Add files via upload

--- day2.py
+++ day2.py
@@ -1,28 +1,14 @@
 import re
 
 
-file = open(r'day2_data2.txt', 'r')
-#data = file.readlines()
+file = open(r'day2_data.txt', 'r')
 passwords = re.findall(r'(\d+)-(\d+)\s(\w):\s(\w+)', file.read())
 valid = 0
 
-
 for i in range(len(passwords)):
-    print(passwords[i])
     valid_check = passwords[i][3].count(passwords[i][2])
 
-    print(valid_check)
-
-    if valid_check >= int(passwords[i][0]) & valid_check <= int(passwords[i][1]):
+    if valid_check >= int(passwords[i][0]) and valid_check <= int(passwords[i][1]):
         valid += 1
-    
 
-"""for tuple in passwords:
-    print(tuple[0], tuple[1], tuple[2], tuple[3])
-    print(passwords[0])
-    #if passwords[tuple]"""
-    
-    
 print('# of valid passwords: ', valid)
-#print(file.read())
-#print(passwords)
